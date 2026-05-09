3abdde1ced750ef3fee300dd9f7b741b8c6d1512
Ranbir <ranbir.webdev@gmail.com>
2026-05-09 23:13:29 +0600

    update multiple.js file

--- js_conditionals/multiple.js
+++ js_conditionals/multiple.js
@@ -24,3 +24,19 @@ if (salary >19000 && height > 69){
 else{
     console.log('vag tui mokbul');
 }
+
+
+var marks = 85;
+var attendance = 90;
+
+if (marks >= 80 && attendance >= 75) {
+    console.log("Scholarship pabe");
+}
+
+
+var rainy = true;
+var umbrella = false;
+
+if (rainy || umbrella) {
+    console.log("Baire jawa jabe");
+}
