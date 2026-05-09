fa30dcb3f96429691ec974c41bb2a9cd4013a7b5
Ranbir <ranbir.webdev@gmail.com>
2026-05-09 23:42:22 +0600

    update multiple.js file

--- js_conditionals/multiple.js
+++ js_conditionals/multiple.js
@@ -40,3 +40,24 @@ var umbrella = false;
 if (rainy || umbrella) {
     console.log("Baire jawa jabe");
 }
+
+
+
+const price = 5600;
+
+if (price >= 5000){
+    //10% discount
+    const discount = price * 10/100;
+    const payAmount = price - discount;
+    console.log(payAmount)
+}
+else if (price >= 3000){
+    //5% discount
+    const discount = price * 5/100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}
+else {
+    console.log(price);
+}
+
