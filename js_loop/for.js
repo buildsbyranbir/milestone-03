de20392a9b46fdf5350a4062aa5c92bfd30b3f83
Ranbir <ranbir.webdev@gmail.com>
2026-05-10 22:29:25 +0600

    update for.js file

--- js_loop/for.js
+++ js_loop/for.js
@@ -14,3 +14,24 @@
 for(let num =0; num <5; num++){
     console.log(num);
 }
+
+
+
+const numbers = [12, 15, 658,45,85]
+console.log(numbers)
+
+for(const num of numbers){
+    console.log(num);
+}
+
+
+
+// for () {
+
+// }
+
+const fruits = ["banana", "jackfruit", "apple", "orange"];
+for (const fruit of fruits){
+    console.log(fruit);
+    console.log("I want to eat")
+}
