6d333d7e8becc15da05a8f11a1168bb4a1ea10db
Ranbir <ranbir.webdev@gmail.com>
2026-05-10 23:07:12 +0600

    update while.js file

--- js_loop/while.js
+++ js_loop/while.js
@@ -1,10 +1,18 @@
+// while loop
+
+// num name er ekta variable nilam ebong er man 0 dilam
 let num = 0;
+
+// jotokkhon num er man 5 er cheye choto thakbe totokkhon loop cholbe
 while(num < 5){
+
+    // console e "looping" ebong num er current value dekhabe
     console.log('looping', num);
-    // num = num + 1;
-    // num += 1;
-    num++;
+
+    // num er sathe 1 jog hocche
+    // mane protibar loop ghurar por num barche
+    // num = num + 1; er short form hocche num += 1;
+    num += 1;
+
+    // num++; o same kaj kore
 }
-
-
-
