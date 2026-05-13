479e3367bc7973fd831edc66ec00a2afedd6e4f5
Ranbir <ranbir.webdev@gmail.com>
2026-05-13 23:29:03 +0600

    add different.js file

---
+++ js_loop/different.js
@@ -0,0 +1,40 @@
+// odd numbers print korar jonno loop
+for(i = 0; i < 20; i++){
+
+    // jodi number ta 2 diye vag korle remainder 1 hoy
+    // tahole bujhte hobe eta odd number
+    if(i % 2 === 1){
+
+        // odd number console a print korbe
+        console.log(i);
+    }
+}
+
+
+
+
+// even numbers print korar jonno loop
+for(i = 0; i < 20; i++){
+
+    // jodi number ta 2 diye vag korle remainder 0 hoy
+    // tahole eta even number
+    if(i % 2 == 0){
+
+        // even number console a print korbe
+        console.log(i);
+    }
+}
+
+
+
+// 1 theke 30 porjonto 5 diye divisible number ber korar loop
+for(let i = 1; i <= 30; i++){
+
+    // jodi number ta 5 diye vag jay
+    // mane remainder 0 hoy
+    if(i % 5 === 0){
+
+        // divisible number print korbe
+        console.log(i);
+    }
+}
