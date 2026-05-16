5721fc1c54af78842896ca87ff8e8e3de0211600
Ranbir <ranbir.webdev@gmail.com>
2026-05-16 21:31:16 +0600

    update object.js file

--- js_string_object/object.js
+++ js_string_object/object.js
@@ -1,29 +1,77 @@
+// primitive type variable
+
+// number type data
 const age = 25;
+
+// string type data
 const school = 'rifle square';
+
+// boolean type data
+// true mane pass koreche
 const isPassed = true;
+
+
+// variable declare kora holeo value deoya hoyni
+// tai default value undefined hobe
 // let isDeveloper;
+
+// undefined print korto
 // console.log(isDeveloper);
 
 
-const subjects = ['math', 'english', 'bangla', 'chemistry']
 
 
-// object
-// non-primitive
+// array holo non-primitive data type
+// ekhane multiple subject list rakha hoyeche
+const subjects = ['math', 'english', 'bangla', 'chemistry'];
+
+
+
+
+
+// object holo non-primitive data type
+// object er moddhe key:value pair thake
 const bottle = {
-    brand : 'Apple',
-    price : 45,
-    color : 'white',
-    isClean : false
-}
 
-// non-primitive
+    // bottle brand
+    brand: 'Apple',
+
+    // bottle price
+    price: 45,
+
+    // bottle color
+    color: 'white',
+
+    // bottle clean kina
+    // false mane clean na
+    isClean: false
+};
+
+
+
+
+
+// arekta object create kora holo
 const subject = {
+
+    // subject name
     name: 'Mathematics',
+
+    // teacher name
     teacher: 'Mr. Rahim',
+
+    // exam date
     examDate: '17-10-2025',
+
+    
+    // nested object
+    // object er vitore arekta object
     emams: {
+
+        // exam name
         name: 'Final Exam',
+
+        // total marks
         marks: 100
     }
-}
+};
